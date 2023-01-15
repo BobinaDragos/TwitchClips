94a1188d1466f37df003c8513d29d0568b071291
Bobină Dragoș-Cătălin <115873660+BobinaDragos@users.noreply.github.com>
2023-01-15 15:03:48 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace RandomClips
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(2, -3);
+            this.axWindowsMediaPlayer1.MaximumSize = new System.Drawing.Size(3840, 2160);
+            this.axWindowsMediaPlayer1.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1920, 1018);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(3840, 2160);
+            this.MinimumSize = new System.Drawing.Size(1280, 720);
+            this.Name = "Form1";
+            this.Text = "Random Clips ©Purra#9993";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace RandomClips
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            int fCount = System.IO.Directory.GetFiles("./Clips", "*", System.IO.SearchOption.TopDirectoryOnly).Length;
+            int [] playlist = new int[fCount];
+            string[] fileListPath = new string[fCount];
+            for (int i = 0; i<fCount;i++)
+            {
+                int x = rand.Next(1, fCount);
+                if (playlist.Contains(x))
+                {
+                }
+                else
+                {
+                    playlist[i] = x;
+                    //MessageBox.Show("" + x);
+                }
+            }
+            int fileIndex = 0;
+                foreach (string fileName in Directory.GetFiles("./Clips", "*", System.IO.SearchOption.TopDirectoryOnly))
+                {
+                fileListPath[fileIndex] = fileName;
+                fileIndex++;
+                
+                }
+            string[] playpaths = new string[fCount];
+            var pClips = axWindowsMediaPlayer1.playlistCollection.newPlaylist("Cips");
+            for (int i = 0; i < fCount; i++)
+            {
+               playpaths[i] = fileListPath[playlist[i]];
+               var mediaItem = axWindowsMediaPlayer1.newMedia(playpaths[i]);
+               pClips.appendItem(mediaItem);
+            }
+
+            axWindowsMediaPlayer1.currentPlaylist = pClips;
+            axWindowsMediaPlayer1.uiMode = "none";
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            axWindowsMediaPlayer1.close();
+        }
+    }
+}
